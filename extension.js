63c5fb82758cd6410fd8dbf320996af533bd0580
Sergey <sershov9193@gmail.com>
2026-04-28 17:02:18 +0300

    Fixed settings parsing.

--- extension.js
+++ extension.js
@@ -171,12 +171,17 @@ function expandConfigValue(value, editor, folder) {
     if (typeof value === "string") {
         const filePath = editor ? editor.document.uri.fsPath : "";
         const folderPath = folder ? folder.uri.fsPath : "";
+        const workspaceConfig = vscode.workspace.getConfiguration(undefined, folder && folder.uri);
 
         return value
             .replace(/\$\{file\}/g, filePath)
             .replace(/\$\{fileBasename\}/g, filePath ? path.basename(filePath) : "")
             .replace(/\$\{workspaceFolder\}/g, folderPath)
-            .replace(/\$\{cwd\}/g, folderPath);
+            .replace(/\$\{cwd\}/g, folderPath)
+            .replace(/\$\{config:([^}]+)\}/g, (_, key) => {
+                const configValue = workspaceConfig.get(key);
+                return configValue === undefined ? "" : String(configValue);
+            });
     }
 
     if (Array.isArray(value)) {
